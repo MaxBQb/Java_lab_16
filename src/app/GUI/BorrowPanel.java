b83875df9479e69225ce4fcea2557dc2cf647dd8
Dedicated407 <64129022+Dedicated407@users.noreply.github.com>
2020-10-31 14:34:34 +0300

    View extended: + ChoiceTable + Application added welcome page + BorrowPanel started
    Main test mod enabled

--- src/app/GUI/BorrowPanel.java
+++ src/app/GUI/BorrowPanel.java
@@ -6,31 +6,88 @@ import java.awt.*;
 public class BorrowPanel extends JFrame {
     JLabel lbl_occupation_info = new JLabel("Список занятых столиков: ");
     JLabel lbl_send_order = new JLabel("Заказ выбранного столика: ");
-    JTextField orders_list = new JTextField(20);
+    JTextArea info_list = new JTextArea(3, 20);
+    JTextArea orders_list = new JTextArea(3, 20);
+    JScrollPane scrollBar;
 
     JButton button_add = new JButton("Добавить"); //Добавление клиента
     JButton button_search = new JButton("Найти заказ");
     JButton button_remove = new JButton("Удалить");
 
     public BorrowPanel() {
-        super("Оформление заказа");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(2, 1));
+        final JFrame frame = new JFrame("Оформление заказа в ресторане");
+        //frame.setSize(900,600);
+        //setLayout(new GridBagLayout());
 
+        scrollBar = new JScrollPane(this.info_list);
+
+/*
         button_add.addActionListener(e -> {
             JOptionPane.showMessageDialog(this,
                     "Ничего не найдено!",
                     "Результаты поиска",
-                    JOptionPane.INFORMATION_MESSAGE
+                    JOptionPane.QUESTION_MESSAGE
             );
         });
+
+        button_add.setSize(1,1);
+        button_add.setPreferredSize(new Dimension( 1, 1));
+        button_add.setMaximumSize(new Dimension(1,1));
+        button_add.setMinimumSize(new Dimension(1,1));
         add(button_add);
 
+        button_search.addActionListener(e -> {
+            JOptionPane.showMessageDialog(this,
+                    "Ничего не найдено!",
+                    "Результаты поиска",
+                    JOptionPane.QUESTION_MESSAGE
+            );
+        });
+        add(button_search);
 
-        pack(); // Уважать окно
-        setLocationRelativeTo(null); // отцентрировать окно
-        setVisible(true);
+        button_remove.addActionListener(e -> {
+            JOptionPane.showMessageDialog(this,
+                    "Ничего не найдено!",
+                    "Результаты поиска",
+                    JOptionPane.QUESTION_MESSAGE
+            );
+        });
+        add(button_remove);
+*/
+
+        JPanel buttonPanel = new JPanel(new GridLayout(3, 1));
+        buttonPanel.add(button_add);
+        buttonPanel.add(button_search);
+        buttonPanel.add(button_remove);
+
+        JPanel east = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.anchor = GridBagConstraints.NORTH; //установка местоположения кнопок
+        gbc.weighty = 1; //Указывает, как распределить дополнительное вертикальное пространство
+        east.add(buttonPanel, gbc);
+
+
+        JPanel west = new JPanel(new GridLayout(2, 2, 0, 10));
+        west.add(lbl_occupation_info);
+        west.add(scrollBar);
+        west.add(lbl_send_order );
+        west.add(orders_list );
+        west.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        frame.add(east, BorderLayout.EAST);
+        frame.add(west);
+        frame.setLocationRelativeTo(null); // отцентрировать окно
+        frame.pack();
+
+        frame.setVisible(true);
     }
 
 }
-
+/*
+{
+            @Override
+            public Dimension getPreferredSize() {
+                return new Dimension(200, 200);
+            }
+        };
+ */
--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,13 @@
 package app;
 
+import app.GUI.Application;
 import app.GUI.BorrowPanel;
+import app.GUI.ChoiceTable;
 
 class Main {
     public static void main(String[] args) {
-        new BorrowPanel();
+        //new Application();
+        //new BorrowPanel();
+        new ChoiceTable();
     }
 }
