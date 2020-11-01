d15258a2ee669a84db374663db78a0a58c4fce13
Бабышев Максим <cmdazaza@gmail.com>
2020-11-01 11:56:43 +0300

    Controller base added

---
+++ src/app/GUI/Controller/cApplication.java
@@ -0,0 +1,10 @@
+package app.GUI.Controller;
+import app.GUI.Application;
+
+public class cApplication {
+    cApplication(Application view) {
+        // Тут код для привязки к кнопкам
+        // каких-то действий
+        // вообще вся динамическая часть окна
+    }
+}
---
+++ src/app/GUI/Controller/cChoiceTable.java
@@ -0,0 +1,10 @@
+package app.GUI.Controller;
+import app.GUI.ChoiceTable;
+
+public class cChoiceTable {
+    cChoiceTable(ChoiceTable view) {
+        // Тут код для привязки к кнопкам
+        // каких-то действий
+        // вообще вся динамическая часть окна
+    }
+}
