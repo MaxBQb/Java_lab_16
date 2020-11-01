229d252901aec7d8eed5911e1e1e26eb1127f649
Бабышев Максим <cmdazaza@gmail.com>
2020-11-01 23:04:19 +0300

    validator extended

---
+++ src/app/Controller/Validators/isNumber.java
@@ -0,0 +1,46 @@
+package app.Controller.Validators;
+
+import javax.swing.*;
+
+public class isNumber extends InputVerifier {
+    int maximum;
+    int minimum;
+
+    public isNumber(int min_value, int max_value) {
+        super();
+        maximum = max_value;
+        minimum = min_value;
+    }
+
+    public isNumber() {
+        this(1, Integer.MAX_VALUE);
+    }
+
+    @Override
+    public boolean verify(JComponent input) {
+        String text = ((JTextArea)input).getText();
+        try {
+            int a = Integer.parseInt(text);
+            if (a < minimum || a > maximum)
+                throw new Exception();
+            return true;
+        } catch (Throwable ignore) {
+            String message = "Поле должно содержать ";
+            if (minimum == 1)
+                message += "натуральное число";
+            else
+                message += "целое число, больше "+minimum;
+
+            if (maximum != Integer.MAX_VALUE)
+                message += ", меньше "+maximum;
+
+            message += '!';
+            JOptionPane.showMessageDialog(null,
+                    message,
+                    "Ошибка!",
+                    JOptionPane.WARNING_MESSAGE
+            );
+        }
+        return false;
+    }
+}
