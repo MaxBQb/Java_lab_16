8eefe793309d5d900cb2514b25b18af85096c0d2
Бабышев Максим <cmdazaza@gmail.com>
2020-11-01 22:10:42 +0300

    Controller: only one instance of pre-order pages can be started at the same time

--- src/app/Controller/cApplication.java
+++ src/app/Controller/cApplication.java
@@ -2,18 +2,25 @@ package app.Controller;
 import app.GUI.AddressGui;
 import app.GUI.Application;
 import app.GUI.ChoiceTable;
+import app.GUI.LogIn;
 
 public class cApplication {
+    private ChoiceTable choiceTable_panel;
+    private LogIn login_panel;
     public cApplication(Application view) {
 
         view.button_table.addActionListener(e -> {
-            new ChoiceTable();
-            view.button_table.setEnabled(false);
+            if (choiceTable_panel != null &&
+                choiceTable_panel.isVisible()
+            ) return;
+            choiceTable_panel = new ChoiceTable();
         });
 
         view.button_online.addActionListener(e -> {
-            new AddressGui();
-            view.button_online.setEnabled(false);
+            if (login_panel != null &&
+                login_panel.isVisible()
+            ) return;
+            login_panel = new LogIn();
         });
     }
 }
