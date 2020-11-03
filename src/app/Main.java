c50ed27ab8eb0ea47f2be1650415a2c79371138e
Бабышев Максим <cmdazaza@gmail.com>
2020-11-03 12:17:26 +0300

    now only main app starts (debug=false)

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,12 +4,5 @@ import app.GUI.*;
 class Main {
     public static void main(String[] args) {
         new Application();
-        new BorrowPanel();
-        new ChoiceTable();
-        new LogIn();
-        new AddressGui();
-        new Order();
-        new DishGui();
-        new DrinkGui();
     }
 }
